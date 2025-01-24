8e668c51152a43a0dcbf4966618fd9ea97e37b70
tanabe0624 <ryo.tnb.bn@gmail.com>
2025-01-25 08:19:07 +0900

    Create item entity. Create item DAO.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
