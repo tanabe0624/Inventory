675e29456c54e00ec2f7144b7c50c68fc88dd911
tanabe0624 <ryo.tnb.bn@gmail.com>
2025-01-25 09:07:27 +0900

    Create repository

--- app/src/main/java/com/example/inventory/data/ItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -16,7 +16,20 @@
 
 package com.example.inventory.data
 
+import kotlinx.coroutines.flow.Flow
+
 /**
  * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
  */
-interface ItemsRepository
+interface ItemsRepository {
+
+    //Retrieve all the items from the the given data source.
+    fun getALlItemsStream(): Flow<List<Item>>
+
+    //Retrieve an item from the given data source that matches with the [id].
+    fun getItemStream(id: Int): Flow<Item?>
+
+    suspend fun insertItem(item: Item)
+    suspend fun deleteItem(item: Item)
+    suspend fun updateItem(item: Item)
+}
